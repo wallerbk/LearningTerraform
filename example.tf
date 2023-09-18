4a6a0d148fb51d425431d87f1b29d742fdcb5e90
Bear <145365562+wallerbk@users.noreply.github.com>
2023-09-18 14:10:09 -0400

    fixed spelling

--- example.tf
+++ example.tf
@@ -11,7 +11,7 @@ provider "linode" {
     # Configuration options
     token = "4aaf8c58bf19148fd9809c1f9e6af65a0572c70bdbf5f9516b96b663fb4c35ee"
 }
-resource "linode_instance" "example_isntance" {
+resource "linode_instance" "example_instance" {
     label       = "example_instance"
     image       = "linode/ubuntu23.04"
     region      = "us-southeast"
