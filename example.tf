3bc0169a93759a255c6b7981628b530bec3ab646
Bear <145365562+wallerbk@users.noreply.github.com>
2023-09-18 14:04:43 -0400

    first example

---
+++ example.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    linode = {
+        source = "linode/linode"
+        version = "2.7.2"
+    }
+  }
+}
+
+provider "linode" {
+    # Configuration options
+    token = "4aaf8c58bf19148fd9809c1f9e6af65a0572c70bdbf5f9516b96b663fb4c35ee"
+}
+resource "linode_instance" "example_isntance" {
+    label       = "example_instance"
+    image       = "linode/ubuntu23.04"
+    region      = "us-southeast"
+    type        = "g6-nanode-1"
+    root_pass   = "39 Black#Cats"
+}
